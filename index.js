28422816204fedf324bfb337011c419e49f30c86
Andrew Wilson <adwilson12901@gmail.com>
2023-11-03 15:16:45 -0400

    lab done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const num1 = 31;
+//write your code here
+//they can all be let or const
+//this does simple multiplication
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
 
-const num2 = 2;
+//Math. is a library
+//Math.floor rounds the number down
+//Math.random returns a random int
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
 
-const multiply = num1 * num2;
+//this does division and returns the remainder
+//in this : 18/7 = 2 w/ remainder 4
+const num3 = 18
+const num4 = 7
+const mod = num3 % num4
+console.log(mod)
 
-console.log(multiply);
+//returns highest number in a set
+const max = Math.max(10, 5, 20, 15)
+console.log(max)
+
